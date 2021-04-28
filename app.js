ca39eae8dbcd1edbd053ffbaf090fe45d8d77485
Spencer Gray <grayspa17@gmail.com>
2021-04-28 09:50:40 -0400

    does this work? idk

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 const express = require('express');
 const request = require('request');
+const generator = require('./public/js/questions.js')
 const app = express();
-const {spawn} = require('child_process');
 var port = process.env.PORT || 3000;
+var questions = [] 
 
 app.set('views', __dirname + '/public/views')
 app.set('view engine', 'ejs')
@@ -11,16 +12,17 @@ app.use(express.static(__dirname + '/public'))
 
 app.post('/:numQuestions', (req, res) => {
 
-    console.log('post')
-    const python = spawn('python', ['python/generate.py', req.params.numQuestions]);
-    python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...');
-        var d = data.toString().split('\n')
-        d.pop()
+    if (questions.length >= req.params.numQuestions) {
 
-        res.send({ data: { found: true, questions: d, length: d.length } } )
-    });
-    // res.send({ data: { found: true, questions: ['Hello', 'World'], length: 2 } } )
+        questions.pop(0)
+        var d = questions.slice(0, req.params.numQuestions)
+        for (var i = 0; i < req.params.numQuestions; i++) {
+            questions.pop(0)
+        }
+        res.send({data: {found: true, questions: d, length: d.length}})
+    } else {
+        res.send({data: {found: false, length: req.params.numQuestions}})
+    }
 });
 
 app.get('/', (req, res) => {
@@ -28,10 +30,10 @@ app.get('/', (req, res) => {
     res.render('question', {data: {found: false}})
 }); 
 
-// app.get('/', (req, res) => {
-//     res.render('test')
-// });
-
 app.listen(port, () => {
   console.log(`Server Open on Port ${port}`)
+  questions = []
+  generator.getQuestions(10000, (list) => {
+      questions = list
+  })
 })
---
+++ public/js/questions.js
@@ -0,0 +1,15 @@
+const {spawn} = require('child_process');
+
+function getQuestions(number, callback) {
+    
+    const python = spawn('python', ['python/generate.py', number]);
+    python.stdout.on('data', function (data) {
+        // console.log('Pipe data from python script ...');
+        var d = data.toString().split('\n')
+        d.pop(0)
+        
+        callback(d)
+    });
+}
+
+module.exports = { getQuestions };
